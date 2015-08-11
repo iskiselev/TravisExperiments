2334c5f2cce0a1a6c399a2682ee555576d535445
ik <igor@kiselev.spb.ru>
2015-08-11 15:43:59 -0700

    Initial.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TravisExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Win32.Registry.SetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\FSharp\3.1\Runtime\v4.0", null, @".");
+        }
+    }
+}
