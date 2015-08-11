56b3c274c7a5002e5719633057f4606f46b70fb4
ik <igor@kiselev.spb.ru>
2015-08-11 16:29:02 -0700

    test it all!

--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
 ﻿namespace TravisExperiments
 {
     using System;
-    using System.IO;
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Microsoft.Win32.Registry.LocalMachine.OpenSubKey("Software").OpenSubKey("Microsoft").OpenSubKey("FSharp").OpenSubKey("3.1")
-                .OpenSubKey("Runtime")
-                .OpenSubKey("v4.0")
-                .GetValue(String.Empty));
+            Console.WriteLine(Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\FSharp\3.1\Runtime\v4.0", null, null));
         }
     }
 }
