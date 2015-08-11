cfbc3807789abd87679cd03e113f296ec2c8fa49
ik <igor@kiselev.spb.ru>
2015-08-11 16:17:46 -0700

    and more

--- Program.cs
+++ Program.cs
@@ -7,12 +7,10 @@
     {
         static void Main(string[] args)
         {
-            Microsoft.Win32.Registry.LocalMachine.CreateSubKey("Software").CreateSubKey("Microsoft").CreateSubKey("FSharp").CreateSubKey("3.1")
-                .CreateSubKey("Runtime")
-                .CreateSubKey("v4.0")
-                .SetValue(String.Empty, @"blah!");
-            var str = File.ReadAllText(@"/tmp/test/LocalMachine/software/microsoft/fsharp/3.1/runtime/v4.0/values.xml");
-            Console.WriteLine(str);
+            Console.WriteLine(Microsoft.Win32.Registry.LocalMachine.OpenSubKey("Software").OpenSubKey("Microsoft").OpenSubKey("FSharp").OpenSubKey("3.1")
+                .OpenSubKey("Runtime")
+                .OpenSubKey("v4.0")
+                .GetValue(String.Empty));
         }
     }
 }
