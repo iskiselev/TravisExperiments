e0b55c5c5b441a7b9bff24e13d4868728b9e25cc
ik <igor@kiselev.spb.ru>
2015-08-11 16:02:24 -0700

    and  more

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace TravisExperiments
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Microsoft.Win32.Registry.SetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\FSharp\3.1\Runtime\v4.0", null, @".");
+            Microsoft.Win32.Registry.LocalMachine.CreateSubKey("Software").CreateSubKey("Microsoft").CreateSubKey("FSharp").CreateSubKey("3.1")
+                .CreateSubKey("Runtime")
+                .CreateSubKey("v4.0")
+                .SetValue(String.Empty, @".");
         }
     }
 }
