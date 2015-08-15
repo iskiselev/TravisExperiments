99dfb1f8132f68c108cfb04f2e0b263b7afe0373
ik <igor@kiselev.spb.ru>
2015-08-14 17:47:03 -0700

    Test Node.

--- Program.cs
+++ Program.cs
@@ -2,36 +2,14 @@
 {
     using System;
     using System.Diagnostics;
+    using System.IO;
     using System.Linq;
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("//arguments//");
-            foreach (var s in args)
-            {
-                Console.WriteLine(s);
-            }
-
-            Console.WriteLine("//endarguments//");
-            if (args.Length > 0 && string.Concat(args).Contains("blahhhhh"))
-            {
-                Console.WriteLine("Hura!");
-            }
-
-            var p = new Process
-            {
-                StartInfo =
-                {
-                    FileName = "/home/travis/build/iskiselev/TravisExperiments/bin/FSharp.Compiler.Tools/tools/fsc.exe",
-                    //Arguments = "./FSharp.Compiler.Tools/tools/fsc.exe",
-                    UseShellExecute = false
-                }
-            };
-            p.Start();
-            p.WaitForExit();
-            Console.WriteLine(p.ExitCode);
+            Console.WriteLine(Environment.ExpandEnvironmentVariables("cmd1.exe"));
         }
     }
 }
