3dd10829ba98beb459f13fce51a3393d969cd381
ik <igor@kiselev.spb.ru>
2015-08-23 10:40:07 -0700

    Sample for nunit/nunit-vs-adapter/issues#77

---
+++ TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TravisExperiments
+{
+    using NUnit.Core;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+             Console.WriteLine("Test executed");
+        }
+    }
+}
